5de78c466855eb447a82c08d0aba3a44aa5afb0b
Rubhan Azeem <rubhanazeem@gmail.com>
2026-06-19 13:59:18 +0200

    Render `Unknown source` on workflow run show pages when commit information is missing
    Co-authored-by: Felix Gerling <fgerling@suse.com>

--- src/api/app/components/workflow_run_header_component.rb
+++ src/api/app/components/workflow_run_header_component.rb
@@ -15,4 +15,13 @@ class WorkflowRunHeaderComponent < WorkflowRunRowComponent
       'text-bg-warning'
     end
   end
+
+  def event_source_link(label_prefix: nil)
+    return 'Unknown source' if event_source_name.blank?
+
+    label = "#{label_prefix}#{event_source_name}"
+    return label if event_source_url.blank?
+
+    link_to(label, event_source_url)
+  end
 end
